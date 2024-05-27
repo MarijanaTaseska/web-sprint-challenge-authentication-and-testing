3f59936d552132ba4ce92f4d84f564dc1949ed24
Marijana Taseska <marijana.taseska@gmail.com>
2024-05-27 00:51:37 -0700

    All endpoints tested and working

--- api/server.test.js
+++ api/server.test.js
@@ -1,21 +1,11 @@
 // Write your tests here
 const db = require('../data/dbConfig')
 const request = require('supertest')
-const jokes = require('../api/jokes/jokes-router')
-const auth = require('../api/auth/auth-router')
 const server = require('../api/server')
 
 beforeAll(async () => {
   await db.migrate.rollback()
   await db.migrate.latest()
-})
-
-let token
-beforeEach(async () => {
-  const response = await request(server)
-    .post('/api/login')
-    .send({ username: 'validUsername', password: 'validPassword' })
- token = response.body.token;
 })
 
 
@@ -30,12 +20,11 @@ describe('[POST] / register', () => {
   test('returns 400 when username is taken', async () => {
     const user = { username: 'hobi', password: 'bibi' }
     await request(server).post('/api/auth/register').send(user); // register once
-    const res = await request(server).post('/api/auth/register').send(user); // try to register again with the same username
+    const res = await request(server).post('/api/auth/register').send(user); // register again with the same username
     expect(res.status).toBe(400);
     expect(res.body).toHaveProperty('message', 'username taken');
   });
 })
-
 
 describe('[POST] / login', () => {
   test('returns 200 on successful login', async () => {
@@ -58,16 +47,23 @@ describe('[POST] / login', () => {
   });
 })
 describe('[GET] / jokes', () => {
+  let token
+beforeEach(async () => {
+  const response = await request(server)
+    .post('/api/auth/login')
+    .send({ username: 'bobi', password: 'bobi' })
+     token = response.body.token;
+})
   test('returns 401 if no token is provided', async () => {
     const res = await request(server).get('/api/jokes')
     expect(res.status).toBe(401)
+    expect(res.body).toHaveProperty('message', 'token required')
   })
   test('returns 200 and jokes when valid token is provided', async () => {
-    const response = await request(server)
+    const res = await request(server)
       .get('/api/jokes')
-      .set('Authorization', `${token}`);
-  
-    expect(response.status).toBe(200);
-    //expect(response.body).toBeDefined();
-  });
+      .set('Authorization', `${token}`)
+    expect(res.status).toBe(200)
+    expect(res.body).toBeDefined()
+  })
 })
